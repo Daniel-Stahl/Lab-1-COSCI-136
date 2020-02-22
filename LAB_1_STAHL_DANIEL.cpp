e3c04ce1fbc0b28a0ca8d96f5dd360fdaf6ca8ca
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2020-02-21 19:44:40 -0800

    enhanced sort algorithm and made 2 classes

--- LAB_1_STAHL_DANIEL.cpp
+++ LAB_1_STAHL_DANIEL.cpp
@@ -9,22 +9,40 @@
 
 using namespace std;
 
-class InventoryItems { // A class for my inventory
+class Item {
 public:
-    vector<InventoryItems>unsortedInventory;
-    vector<InventoryItems*>sortedInventory;
     string itemID;
     string itemName;
     int itemQuantity;
     double itemPrice;
     
-    InventoryItems(){};
-    InventoryItems(string itemID, string itemName, int itemQuantity, double itemPrice) {
+    Item(){};
+    Item(string itemID, string itemName, int itemQuantity, double itemPrice) {
+        //One liners
         this->itemID = itemID;
         this->itemName = itemName;
         this->itemQuantity = itemQuantity;
         this->itemPrice = itemPrice;
     };
+};
+
+class Inventory {
+public:
+    
+    vector<Item>unsortedInventory;
+    vector<Item*>sortedInventory;
+//    string itemID;
+//    string itemName;
+//    int itemQuantity;
+//    double itemPrice;
+    
+//    Inventory(){};
+//    Inventory(string itemID, string itemName, int itemQuantity, double itemPrice) {
+//        itemID = itemID;
+//        itemName = itemName;
+//        this->itemQuantity = itemQuantity;
+//        this->itemPrice = itemPrice;
+//    };
     
     void PrintUnsortedInventory() { // Prints the unsorted array
         cout << "Unsorted inventory:" << endl;
@@ -37,20 +55,19 @@ public:
             cout << left << setw(10)<< unsortedInventory.at(x).itemID
             << setw(13) << unsortedInventory.at(x).itemName
             << setw(10) << unsortedInventory.at(x).itemQuantity
-            << "$" << unsortedInventory.at(x).itemPrice << endl;
-            cout << "\n";
+            << "$" << unsortedInventory.at(x).itemPrice << "\n" << endl;
         }
     };
     
-    void Swap(InventoryItems*& itemA, InventoryItems*& itemB) { // Swaps items in the array
-        InventoryItems* temp = itemA;
-        itemA = itemB;
-        itemB = temp;
-    }
+//    void Swap(Inventory*& itemA, Inventory*& itemB) { // Swaps items in the array
+//        Inventory* temp = itemA;
+//        itemA = itemB;
+//        itemB = temp;
+//    }
     
     void Sort(int sortChoice) { // Sorts array of addresses by user input
         bool sortInventory = true;
-        
+        bool toSwap=false;
         while (sortInventory) {
             sortInventory = false;
             
@@ -59,35 +76,48 @@ public:
                 switch (sortChoice) {
                     case 1:
                         // Item ID
-                        if (sortedInventory.at(x)->itemID > sortedInventory.at(x+1)->itemID) {
-                            Swap(sortedInventory.at(x), sortedInventory.at(x+1));
-                            sortInventory = true;
-                        }
+//                        if (sortedInventory.at(x)->itemID > sortedInventory.at(x+1)->itemID) {
+//                            Swap(sortedInventory.at(x), sortedInventory.at(x+1));
+//                            sortInventory = true;
+//                        }
+                        toSwap = sortedInventory.at(x)->itemID > sortedInventory.at(x+1)->itemID;
                         break;
                     case 2:
                         // Product Name
-                        if (sortedInventory.at(x)->itemName > sortedInventory.at(x+1)->itemName) {
-                            Swap(sortedInventory.at(x), sortedInventory.at(x+1));
-                            sortInventory = true;
-                        }
+//                        if (sortedInventory.at(x)->itemName > sortedInventory.at(x+1)->itemName) {
+//                            Swap(sortedInventory.at(x), sortedInventory.at(x+1));
+//                            sortInventory = true;
+//                        }
+                        toSwap = sortedInventory.at(x)->itemName > sortedInventory.at(x+1)->itemName;
                         break;
                     case 3:
                         // QTY
-                        if (sortedInventory.at(x)->itemQuantity > sortedInventory.at(x+1)->itemQuantity) {
-                            Swap(sortedInventory.at(x), sortedInventory.at(x+1));
-                            sortInventory = true;
-                        }
+//                        if (sortedInventory.at(x)->itemQuantity > sortedInventory.at(x+1)->itemQuantity) {
+//                            Swap(sortedInventory.at(x), sortedInventory.at(x+1));
+//                            sortInventory = true;
+//                        }
+                        toSwap = sortedInventory.at(x)->itemQuantity > sortedInventory.at(x+1)->itemQuantity;
                         break;
                     case 4:
                         // Price
-                        if (sortedInventory.at(x)->itemPrice > sortedInventory.at(x+1)->itemPrice) {
-                            Swap(sortedInventory.at(x), sortedInventory.at(x+1));
-                            sortInventory = true;
-                        }
+//                        if (sortedInventory.at(x)->itemPrice > sortedInventory.at(x+1)->itemPrice) {
+//                            Swap(sortedInventory.at(x), sortedInventory.at(x+1));
+//                            sortInventory = true;
+//                        }
+                        toSwap = sortedInventory.at(x)->itemPrice > sortedInventory.at(x+1)->itemPrice;
                         break;
-                    default:
-                        break;
+                 
                 }
+                
+                if(toSwap)
+                {
+                    Item* temp = sortedInventory.at(x);
+                    sortedInventory.at(x) = sortedInventory.at(x+1);
+                    sortedInventory.at(x+1) = temp;
+                    //toSwap = false;
+                    sortInventory = true;
+                }
+                    
             }
         }
         
@@ -130,8 +160,8 @@ public:
                 cout << left << setw(10)<< "Item ID" <<
                 setw(13) << "Item Name" <<
                 setw(10) << "Quantity" <<
-                setw(10) << "Price" << endl;
-                cout << left << setw(10)<< unsortedInventory.at(y).itemID
+                setw(10) << "Price\n"
+                        << left << setw(10)<< unsortedInventory.at(y).itemID
                 << setw(13) << unsortedInventory.at(y).itemName
                 << setw(10) << unsortedInventory.at(y).itemQuantity
                 << "$" << unsortedInventory.at(y).itemPrice << "\n" << endl;
@@ -163,7 +193,10 @@ public:
 };
 
 int main() {
-    InventoryItems inventoryItems;
+    vector<Item>unsortedInventory;
+    vector<Item*>sortedInventory;
+    Inventory inventory;
+    Item item;
     ifstream inFile;
     string itemID;
     string itemName;
@@ -174,6 +207,8 @@ int main() {
     string searchInventory;
     
     
+    // File stuff goes in Inventory with a function
+    
     inFile.open("/Users/stahl/Desktop/Pierce College/COSCI 136/lab 1 refresher/data.txt");
     
     if(!inFile) {
@@ -183,27 +218,28 @@ int main() {
         cout << "File ready!" << endl;
     }
     
-    while(!inFile.eof() && inventoryItems.unsortedInventory.size() != 10) {
+    while(!inFile.eof() && inventory.unsortedInventory.size() != 10) { // Check if vector is full
         inFile >> itemID >> itemName >> itemQuantity >> itemPrice;
         
         for (int x = 0; x < itemName.size(); x++) {
             itemName[x] = tolower(itemName[x]);
         }
         
-        inventoryItems.unsortedInventory.push_back(InventoryItems(itemID, itemName, itemQuantity, itemPrice));
+        inventory.unsortedInventory.push_back(Item(itemID, itemName, itemQuantity, itemPrice));
         
-        if (inventoryItems.unsortedInventory.size() == 10) {
+        if (inventory.unsortedInventory.size() == 10) {
             cout << "This program can only process 10 items at a time" << endl;
         }
     }
     
     inFile.close();
     
-    for (int x = 0; x < inventoryItems.unsortedInventory.size(); x++) {
-        inventoryItems.sortedInventory.push_back(&inventoryItems.unsortedInventory.at(x));
+    // try putting this after push_back unsorted
+    for (int x = 0; x < inventory.unsortedInventory.size(); x++) {
+        inventory.sortedInventory.push_back(&inventory.unsortedInventory.at(x));
     }
     
-    // Menu Starts
+    // put menu in Inventory via function
     cout << "Hello, please choose from the menu below\n" << endl;
     
     do {
@@ -225,7 +261,7 @@ int main() {
             switch (userSelection) {
                 case 1:
                     // Print unsorted inventory
-                    inventoryItems.PrintUnsortedInventory();
+                    inventory.PrintUnsortedInventory();
                     break;
                 case 2:
                     // Print inventory sorted by user choice
@@ -242,7 +278,7 @@ int main() {
                         cin.ignore(100, '\n');
                         cout << "\nSorry that is not one of the selections from the menu, please try again.\n" << endl;
                     } else {
-                        inventoryItems.Sort(userSortSelection);
+                        inventory.Sort(userSortSelection);
                     }
                     
                     break;
@@ -250,11 +286,11 @@ int main() {
                     // Search product by ID or name
                     cout << "\nPlease type the product you want to search?" << endl;
                     cin >> searchInventory;
-                    inventoryItems.SearchInventory(searchInventory);
+                    inventory.SearchInventory(searchInventory);
                     break;
                 case 4:
                     // Print inventory details
-                    inventoryItems.PrintInventoryReport();
+                    inventory.PrintInventoryReport();
                     break;
                 default:
                     cout << "\nSorry that is not one of the selections from the menu, please try again.\n" << endl;
