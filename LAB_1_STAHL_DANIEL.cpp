0af111c9b52abcaf3510d212102c477aeff7034f
Daniel Stahl <danny.stahl@gmail.com>
2020-02-23 13:01:35 -0800

    incorporated sub menu int0 proper functions

--- LAB_1_STAHL_DANIEL.cpp
+++ LAB_1_STAHL_DANIEL.cpp
@@ -66,58 +66,76 @@ public:
         }
     }
     
-    void Sort(int sortChoice) { // Sorts array of addresses by user input
-        bool sortInventory = true;
-        bool toSwap = false;
-        
-        while (sortInventory) {
-            sortInventory = false;
+    void Sort() { // Sorts array of addresses by user input
+        // Get user to go back to menu
+        int userSortSelection;
+        do {
+            cout << "How would you like to sort the inventory? (enter 5 to go back to the main menu)\n" << "1) item ID\n" << "2) item name\n" << "3) quantity on hand\n" << "4) price\n" << "5) Main Menu" << endl;
+            cout << "Enter a number: ";
+            cin >> userSortSelection;
+            cout << "\n";
             
-            for (int x = 0; x < sortedInventory.size()-1; x++) {
-                switch (sortChoice) {
-                    case 1:
-                        // Item ID
-                        toSwap = sortedInventory.at(x)->itemID > sortedInventory.at(x+1)->itemID;
-                        break;
-                    case 2:
-                        // Product Name
-                        toSwap = sortedInventory.at(x)->itemName > sortedInventory.at(x+1)->itemName;
-                        break;
-                    case 3:
-                        // QTY
-                        toSwap = sortedInventory.at(x)->itemQuantity > sortedInventory.at(x+1)->itemQuantity;
-                        break;
-                    case 4:
-                        // Price
-                        toSwap = sortedInventory.at(x)->itemPrice > sortedInventory.at(x+1)->itemPrice;
-                        break;
+            if (!cin || userSortSelection > 5) { // Checks if user input is valid
+                cin.clear();
+                cin.ignore(100, '\n');
+                cout << "Sorry that is not one of the selections from the menu, please try again.\n";
+            } else {
+                // Sort here
+                bool sortInventory = true;
+                bool toSwap = false;
+                
+                while (sortInventory) {
+                    sortInventory = false;
+                    
+                    for (int x = 0; x < sortedInventory.size()-1; x++) {
+                        switch (userSortSelection) {
+                            case 1:
+                                // Item ID
+                                toSwap = sortedInventory.at(x)->itemID > sortedInventory.at(x+1)->itemID;
+                                break;
+                            case 2:
+                                // Product Name
+                                toSwap = sortedInventory.at(x)->itemName > sortedInventory.at(x+1)->itemName;
+                                break;
+                            case 3:
+                                // QTY
+                                toSwap = sortedInventory.at(x)->itemQuantity > sortedInventory.at(x+1)->itemQuantity;
+                                break;
+                            case 4:
+                                // Price
+                                toSwap = sortedInventory.at(x)->itemPrice > sortedInventory.at(x+1)->itemPrice;
+                                break;
+                        }
+                        
+                        if(toSwap) {
+                            Item* temp = sortedInventory.at(x);
+                            sortedInventory.at(x) = sortedInventory.at(x+1);
+                            sortedInventory.at(x+1) = temp;
+                            sortInventory = true;
+                        }
+                    }
                 }
                 
-                if(toSwap) {
-                    Item* temp = sortedInventory.at(x);
-                    sortedInventory.at(x) = sortedInventory.at(x+1);
-                    sortedInventory.at(x+1) = temp;
-                    sortInventory = true;
+                if (userSortSelection != 5) {
+                    PrintSortOutput();
                 }
             }
-        }
-
-        PrintSortOutput();
+        } while (userSortSelection != 5);
     }
     
     void SearchInventory() { // Searches array for matching item ID or item name
         string userSearch;
         bool searchFound = false;
         
-        while (userSearch != "back") {
-            cout << "\nPlease type the ID or product name you want to search for\n" << "(type back to return to the main menu)\n" << "Search: ";
+        while (userSearch != "main") {
+            cout << "Please type the ID or product name you want to search for\n" << "(type main to return to the main menu)\n" << "Search: ";
             cin >> userSearch;
             
             LowercaseString(userSearch);
             
             searchFound = false;
             
-            if (userSearch != "back") {
+            if (userSearch != "main") {
                 for (int y = 0; y < unsortedInventory.size(); y++) {
                     if (userSearch == unsortedInventory.at(y).itemID) {
                         cout << "\nFound a match in item ID\n";
@@ -161,8 +179,7 @@ public:
     }
     
     void PrintUnsortedInventory() {
-        cout << "Unsorted inventory:\n";
-        cout << left << setw(10)<< "Item ID" << setw(13) << "Item Name" << setw(10) << "Quantity" << "Price\n";
+        cout << "Unsorted inventory:\n" << left << setw(10)<< "Item ID" << setw(13) << "Item Name" << setw(10) << "Quantity" << "Price\n";
         
         for (int x = 0; x < unsortedInventory.size(); x++) {
             cout << left << setw(10)<< unsortedInventory.at(x).itemID << setw(13) << unsortedInventory.at(x).itemName << setw(10) << unsortedInventory.at(x).itemQuantity << "$" << unsortedInventory.at(x).itemPrice << "\n";
@@ -171,7 +188,7 @@ public:
     };
     
     void PrintSortOutput() { // Prints the output for sorted inventory
-        cout << left << setw(10)<< "Item ID" << setw(13) << "Item Name" << setw(10) << "Quantity" << "Price\n";
+        cout << "Sorted inventory:\n" << left << setw(10)<< "Item ID" << setw(13) << "Item Name" << setw(10) << "Quantity" << "Price\n";
         
         for (int x = 0; x < sortedInventory.size(); x++) {
             cout << left << setw(10)<< sortedInventory.at(x)->itemID << setw(13) << sortedInventory.at(x)->itemName << setw(10) << sortedInventory.at(x)->itemQuantity << "$" << sortedInventory.at(x)->itemPrice << "\n";
@@ -195,62 +212,41 @@ int main() {
 
 void MainMenu(Inventory inventory) {
     int userSelection;
-    int userSortSelection;
     string searchInventory;
     
     cout << "Hello, please choose from the menu below\n" << endl;
     
     do {
-        cout << "1) Print unsorted inventory\n"
-        << "2) Print inventory sorted\n"
-        << "3) Look up item by ID or name\n"
-        << "4) Print report of inventory\n"
-        << "5) Quit program" << endl;
+        cout << "Main Menu\n" << "1) Print unsorted inventory\n" << "2) Print inventory sorted\n" << "3) Look up item by ID or name\n" << "4) Print report of inventory\n" << "5) Quit program" << endl;
         cout << "Enter a number: ";
         cin >> userSelection;
+        cout << "\n";
         
         if (!cin || userSelection > 5) { // Checks if user inout is valid
             cin.clear();
             cin.ignore(100, '\n');
-            cout << "\nSorry that is not one of the selections from the menu, please try again.\n" << endl;
+            cout << "Sorry that is not one of the selections from the menu, please try again." << endl;
         } else {
-            
             // Switch menu
             switch (userSelection) {
                 case 1:
-                    // Print unsorted inventory
+                    // Prints unsorted inventory
                     inventory.PrintUnsortedInventory();
                     break;
                 case 2:
-                    // Print inventory sorted by user choice
-                    // Get user to go back to menu
-                    cout << "\nHow would you like to sort the inventory?\n"
-                    << "1) item ID\n"
-                    << "2) item name\n"
-                    << "3) quantity on hand\n"
-                    << "4) price" << endl;
-                    cout << "Enter a number: ";
-                    cin >> userSortSelection;
-                    
-                    if (!cin || userSortSelection > 4) { // Checks if user input is valid
-                        cin.clear();
-                        cin.ignore(100, '\n');
-                        cout << "\nSorry that is not one of the selections from the menu, please try again.\n" << endl;
-                    } else {
-                        inventory.Sort(userSortSelection);
-                    }
+                    // Prints inventory sorted by user choice
+                    inventory.Sort();
                     
                     break;
                 case 3:
-                    // Search product by ID or name
+                    // Searchs product by ID or name
                     inventory.SearchInventory();
                     break;
                 case 4:
-                    // Print inventory details
+                    // Prints inventory details
                     inventory.PrintInventoryReport();
                     break;
             }
         }
-        
     } while (userSelection != 5);
 }
